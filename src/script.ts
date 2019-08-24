3ae6fce933fd25e65c5b8c0a7066e8f2474b4f77
darumock <darumock@gmail.com>
2019-08-25 01:00:33 +0900

    Create LiteScroll class

---
+++ src/script.ts
@@ -0,0 +1,42 @@
+interface Options {
+  trigger?: string
+  duration?: number
+}
+
+export default class LiteScroll {
+  private options: Options;
+  private requestFrame: number;
+  private startTime: number;
+  private startScrollY: number;
+  private endScrollY: number;
+
+  constructor(options?: Options) {
+    this.options = {
+      trigger: options.trigger ? options.trigger : '.litescroll-trigger',
+      duration: options.duration ? options.duration : 1000
+    };
+    this.mountClickEvent();
+  }
+
+  private mountClickEvent() {
+    document.querySelectorAll(this.options.trigger).forEach((el) => {
+      el.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const scrollToEl = document.getElementById(el.getAttribute('href'));
+
+        this.startTime = new Date().getTime();
+        this.startScrollY = window.scrollY;
+        this.endScrollY = this.getOffset(scrollToEl).top - this.startScrollY;
+      });
+    });
+  }
+
+  private getOffset(el: HTMLElement) {
+    const box = el.getBoundingClientRect();
+    return {
+      top: box.top + window.pageYOffset - document.documentElement.clientTop,
+      left: box.left + window.pageXOffset - document.documentElement.clientLeft
+    }
+  }
+}
