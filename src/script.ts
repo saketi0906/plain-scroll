7ce3463836864e86c7119a8ca6f72ff7e9947222
darumock <darumock@gmail.com>
2019-08-27 00:44:11 +0900

    Confirmed that the animation works

--- src/script.ts
+++ src/script.ts
@@ -3,14 +3,14 @@ interface Options {
   duration?: number;
 }
 
-export default class LiteScroll {
+class LiteScroll {
   private options: Options;
   private requestFrame: number;
   private startTime: number;
   private startScrollY: number;
   private endScrollY: number;
 
-  constructor(options?: Options) {
+  constructor(options: Options = {}) {
     this.options = {
       trigger: options.trigger ? options.trigger : '.litescroll-trigger',
       duration: options.duration ? options.duration : 1000
@@ -19,37 +19,34 @@ export default class LiteScroll {
   }
 
   private mountClickEvent() {
-    const triggerElements = document.getElementsByClassName(this.options.trigger);
+    const triggerElements = document.querySelectorAll(this.options.trigger);
     Array.from(triggerElements).forEach((el) => {
       el.addEventListener('click', (event) => {
         event.preventDefault();
-
-        const scrollToEl = document.getElementById(el.getAttribute('href'));
-
+        const scrollToEl = document.getElementById(el.getAttribute('href').replace('#', ''));
         this.startTime = new Date().getTime();
         this.startScrollY = window.scrollY;
         this.endScrollY = this.getOffset(scrollToEl).top - this.startScrollY;
+        this.animate();
       });
     });
   }
 
   private animate() {
-    var timeElapsed = new Date().getTime() - this.startTime;
-    var isTimeOver = timeElapsed >= this.options.duration;
-    var move = this.calcMoveAmount(timeElapsed);
+    const timeElapsed = new Date().getTime() - this.startTime;
+    const isTimeOver = timeElapsed >= this.options.duration;
+    const move = this.calcMoveAmount(timeElapsed);
     window.scrollTo(0, move);
-    
+
     if (isTimeOver) {
       window.cancelAnimationFrame(this.requestFrame);
     } else {
-      this.requestFrame = window.requestAnimationFrame(function () {
-        return this.animate();
-      });
+      this.requestFrame = window.requestAnimationFrame(() => this.animate());
     }
   }
 
   private calcMoveAmount(timeElapsed) {
-    var processingAmount = timeElapsed / this.options.duration > 1.0 ? 1.0 : timeElapsed / this.options.duration;
+    let processingAmount = timeElapsed / this.options.duration > 1.0 ? 1.0 : timeElapsed / this.options.duration;
     return processingAmount * this.endScrollY + this.startScrollY;
   }
 
@@ -61,3 +58,5 @@ export default class LiteScroll {
     };
   }
 }
+
+new LiteScroll();
